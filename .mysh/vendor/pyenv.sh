bea58959bb105bff9ce0350c8044faed861d96fe
matooy <itunomanica@gmail.com>
2018-05-27 00:32:53 +0900

    fixed pyenv and rbenv bootstrap

--- .mysh/vendor/pyenv.sh
+++ .mysh/vendor/pyenv.sh
@@ -1,5 +1,25 @@
 echo "  Loading pyenv.sh"
 
+# If nvim fails to find python's host,
+# Check your execution path and pip package version are compatible with your python.
+# Sometimes, pip cache and installation will be corrupted
+# when your first installation is shit.(You are noob. R.I.P.)
+# Just force-reinstall these messed things.
+# This took 2 or 3 hours from me.
+#
+# pyenv uninstall YOUR_VER2_PYTHON_ENV
+# pyenv virtualenv YOUR_VER2_PYTHON_NUM YOUR_VER2_PYTHON_ENV
+# pyenv activate YOUR_VER2_PYTHON_ENV
+# pip install --no-cache-dir --upgrade --force-reinstall neovim
+# pyenv uninstall YOUR_VER3_PYTHON_ENV
+# pyenv virtualenv YOUR_VER3_PYTHON_NUM YOUR_VER3_PYTHON_ENV
+# pyenv activate YOUR_VER3_PYTHON_ENV
+# pip3 install --no-cache-dir --upgrade --force-reinstall neovim
+# eval "$(pyenv init -)"
+# eval "$(pyenv virtualenv-init -)"
+
+
+
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
 export PATH="$PYENV_ROOT/shims:$PATH"
@@ -11,8 +31,9 @@ else
 fi
 
 if [ -d "$(pyenv root)""/plugins/pyenv-virtualenv" ]; then
-  echo "  virtualenv/ directory is already there."
+  echo "  pyenv/virtualenv directory is already there."
 else
+  mkdir -p "${PYENV_ROOT}""/pyenv-virtualenv"
   $(git clone https://github.com/yyuu/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv 2> /dev/null)
 fi
 
--- .mysh/vendor/rbenv.sh
+++ .mysh/vendor/rbenv.sh
@@ -4,11 +4,14 @@ echo "  Loading rbenv.sh"
 export RBENV_ROOT="$HOME/.rbenv"
 export PATH="${RBENV_ROOT}/bin:$PATH"
 
-if ! type "rbenv" > /dev/null; then
+if type "rbenv" > /dev/null; then
+  echo "rbenv is already installed."
+else
   $(git clone https://github.com/rbenv/rbenv.git "${RBENV_ROOT}" 2>/dev/null)
 fi
 
 if [ -d "${RBENV_ROOT}""/plugins" ]; then
+  echo "rbenv/plugins directory is already there."
 else
   mkdir -p "${RBENV_ROOT}""/plugins"
   $(git clone https://github.com/rbenv/ruby-build.git "${RBENV_ROOT}""/plugins/ruby-build" 2>/dev/null)
