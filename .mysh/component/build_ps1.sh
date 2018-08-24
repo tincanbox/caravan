6883bcbc5396a90551a86ed4ee3561a49f6d255f
matooy <itunomanica@gmail.com>
2018-08-24 16:37:55 +0900

    fixed ps1 more

--- .mysh/component/build_ps1.sh
+++ .mysh/component/build_ps1.sh
@@ -1,4 +1,3 @@
-
 code="$HOME/.mysh/bin"
 for file in git-completion.bash git-prompt.sh; do
   if [ -e "${code}/${file}" ]; then
@@ -17,4 +16,4 @@ export GIT_PS1_SHOWDIRTYSTATE=true
 export GIT_PS1_SHOWUNTRACKEDFILES=true
 export GIT_PS1_SHOWSTASHSTATE=true
 export GIT_PS1_SHOWUPSTREAM=auto
-export PS1="\[\033[1;32m\]\$(date +%Y/%m/%d_%H:%M:%S)\[\033[0m\] \[\033[33m\]\u@\H:\w\[\033[0m\]\[\033[36m\]\$(__git_ps1)\[\033[00m\]\n\$ "
+export PS1="\[\033[1;32m\]\$(date +%Y/%m/%d_%H:%M:%S)\[\033[0m\] \[\033[33m\]\u\[\033[37m\]@\[\033[36m\]\H:\[\033[33m\]\w\[\033[0m\]\[\033[36m\]\$(__git_ps1)\[\033[00m\]\n\$ "
