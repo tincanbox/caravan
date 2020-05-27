31b67b17ca1fd4602ec49e9075e925d292cd86fd
unknown <tontincanbox@gmail.com>
2020-05-27 16:38:54 +0900

    rmvd python host prog

--- burden/.mysh/vendor/nvim.sh
+++ burden/.mysh/vendor/nvim.sh
@@ -1,7 +1,6 @@
 if type "nvim" 2>&1 1>/dev/null; then
   echo "Loading nvim config"
   alias vim='nvim'
-  let g:python3_host_prog = "/usr/bin/python3"
   export NVIM_LISTEN_ADDRESS="$HOME/.vim/log/listen"
   export NVIM_PYTHON_LOG_FILE="$HOME/.vim/log/log"
   export NVIM_PYTHON_LOG_LEVEL=DEBUG
