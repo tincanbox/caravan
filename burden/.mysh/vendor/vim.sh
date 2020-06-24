1987f3922e26ee8372385820402f1849cf8056fe
unknown <tontincanbox@gmail.com>
2020-06-24 18:04:08 +0900

    fixed vim inst

--- burden/.mysh/vendor/vim.sh
+++ burden/.mysh/vendor/vim.sh
@@ -2,6 +2,6 @@ if type "nvim" 2>&1 1>/dev/null; then
   echo "Loading nvim config"
   alias vim='nvim'
   export NVIM_LISTEN_ADDRESS="$HOME/.vim/log/listen"
-  export NVIM_PYTHON_LOG_FILE="$HOME/.vim/log/log"
+  export NVIM_PYTHON_LOG_FILE="$HOME/.vim/log/pylog"
   export NVIM_PYTHON_LOG_LEVEL=DEBUG
 fi
